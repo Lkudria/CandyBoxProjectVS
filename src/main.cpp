4a774354ef02e8bffbc237db0a70deb76f1f2ffc
Lkudria <91527995+Lkudria@users.noreply.github.com>
2021-11-12 12:27:26 +0100

    adding functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,61 @@
 #include <Arduino.h>
-// include the Servo library
 #include <Servo.h>
+#include <runtimer.h>
 
-//servo int
-Servo myServo;  // create a servo object
-int servoAngle=0; // variable to hold the angle for the servo motor
+#define photoSensorPin 2             // photo Sensor on I/O pin 2
+#define servoMotorPin 9              // servo motor control on I/O 9
+#define servoOpenPosition 120        // servo value for open
+#define servoClosedPosition 0        // servo value for closed
 
-//lasor int:
-int photoSensorPin=2;
-int photoSensorVal=0;
-int previousPhotosState=0;
+static constexpr int unlockTime{1000};        // how long to open for 1 candy [ms]
+bool isLocked{true};                          // is the lock closed (true) or open (false)
+int cardWasPresent = 0;                   // remembers if there was a card present the previous time we checked it
+int cardIsPresent;
+singleTimer openLockTimer;                    // timer controlling how long to open the lock for 1 candy
+Servo myServo;                                // create a servo object
+
+bool cardInserted() {        // returns 'true' if a card insert is detected
+    bool result = false;
+    int cardIsPresent = analogRead(photoSensorPin);
+    Serial.println(cardIsPresent);
+    if (cardWasPresent==0 && cardIsPresent>20) {
+        Serial.println("card inserted");
+        result = true;
+    }
+    else {
+        //Serial.println("card removed");
+    }
+    cardWasPresent = cardIsPresent;
+    return result;
+}
+
+void lock() {
+    isLocked = true;                           // remember we are locked
+    myServo.write(servoClosedPosition);        // set motor to locking position
+    Serial.println("locking");                 // log a msg
+}
+
+void unlock() {
+    isLocked = false;                        // remember we are unlocked
+    myServo.write(servoOpenPosition);        // set motor to open position
+    Serial.println("unlocking");             // log a msg
+}
 
 void setup() {
-  Serial.begin(9600); // open a serial connection to your computer
-  myServo.attach(9); // attaches the servo on pin 9 to the servo object
-  
+    Serial.begin(115200);                         // open a serial connection to your computer, for monitoring
+    //pinMode(photoSensorPin, INPUT_PULLUP);        // set card sensor pin to input, with pullup resistor
+    myServo.attach(servoMotorPin);                // attaches the servo on pin 9 to the servo object
+    lock();
 }
 
 void loop() {
-
-int currentPhotosState = analogRead(photoSensorPin);
-  
-  // servo motor
-   myServo.write(servoAngle);
-   Serial.print("Servo angle:");
-   Serial.println(servoAngle);
-
-   // laser
-   photoSensorVal= analogRead(photoSensorPin);
-   Serial.print("Photosensor: ");
-   Serial.println(photoSensorVal);
-
-if (previousPhotosState==0&&currentPhotosState>500) {  
-    
-    Serial.println("accepted");
-  // set the servo position
-   servoAngle=120;
-   myServo.write(servoAngle);
-   delay(1000); //DELAY TO REPLACE
-   //accepted==true;
-   servoAngle=0;
-   myServo.write(servoAngle);
-   delay(2000); //DELAY TO REPLACE
-}
-
-else if (previousPhotosState>500){
-    
-    Serial.println("declined");
-    servoAngle=0;
-    myServo.write(servoAngle);
-}
-
-previousPhotosState = currentPhotosState;
+    bool isCardInserted = cardInserted();        // check if card is inserted
+    if (isLocked && isCardInserted) {            // when we are locked and a card insert is detected
+        unlock();                                // open the lock
+        openLockTimer.start(unlockTime);         // start a timer
+    }
+    if (openLockTimer.expired()) {        // after some time
+        lock();                           // close the lock
+        //delay(2000);  is not needed
+    }
 }
